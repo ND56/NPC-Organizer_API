3649b599b71e749b99bd812c393714f7cc67e418
ND56 <ndrew56@gmail.com>
2018-03-11 00:44:36 -0500

    After MANY hours of trial and error, finally improved the search-by-class functionality and made it more robust and sophisticated. Cleric now successfully returns tempest Poodle.

--- app/controllers/npcs_controller.rb
+++ app/controllers/npcs_controller.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
+# require 'pry'
+require '/Users/Kayla/Desktop/Nick Programming Folder/Project #2 - NPC Organizer/API - NPC Organizer/db/race_class_arrays.rb'
+# require 'race_class_arrays'
+
 class NpcsController < ProtectedController
   before_action :set_npc, only: %i[update destroy]
 
@@ -39,6 +45,39 @@ class NpcsController < ProtectedController
     else
       render json: @npc.errors, status: :unprocessable_entity
     end
+
+    # adding unique classes to appropriate class arrays for search purposes
+    npc_class = @npc['dnd_class']
+    npc_class_arr = npc_class.split
+    if !(RaceClassArrays::ARTIFICER & npc_class_arr).empty?
+      RaceClassArrays::ARTIFICER.push(npc_class)
+    elsif !(RaceClassArrays::BARBARIAN & npc_class_arr).empty?
+      RaceClassArrays::BARBARIAN.push(npc_class)
+    elsif !(RaceClassArrays::BARD & npc_class_arr).empty?
+      RaceClassArrays::BARD.push(npc_class)
+    elsif !(RaceClassArrays::CLERIC & npc_class_arr).empty?
+      RaceClassArrays::CLERIC.push(npc_class)
+    elsif !(RaceClassArrays::DRUID & npc_class_arr).empty?
+      RaceClassArrays::DRUID.push(npc_class)
+    elsif !(RaceClassArrays::FIGHTER & npc_class_arr).empty?
+      RaceClassArrays::FIGHTER.push(npc_class)
+    elsif !(RaceClassArrays::MONK & npc_class_arr).empty?
+      RaceClassArrays::MONK.push(npc_class)
+    elsif !(RaceClassArrays::MYSTIC & npc_class_arr).empty?
+      RaceClassArrays::MYSTIC.push(npc_class)
+    elsif !(RaceClassArrays::PALADIN & npc_class_arr).empty?
+      RaceClassArrays::PALADIN.push(npc_class)
+    elsif !(RaceClassArrays::RANGER & npc_class_arr).empty?
+      RaceClassArrays::RANGER.push(npc_class)
+    elsif !(RaceClassArrays::ROGUE & npc_class_arr).empty?
+      RaceClassArrays::ROGUE.push(npc_class)
+    elsif !(RaceClassArrays::SORCERER & npc_class_arr).empty?
+      RaceClassArrays::SORCERER.push(npc_class)
+    elsif !(RaceClassArrays::WARLOCK & npc_class_arr).empty?
+      RaceClassArrays::WARLOCK.push(npc_class)
+    elsif !(RaceClassArrays::WIZARD & npc_class_arr).empty?
+      RaceClassArrays::WIZARD.push(npc_class)
+    end
   end
 
   # PATCH/PUT /npcs/1
@@ -71,9 +110,70 @@ class NpcsController < ProtectedController
 
   def search_by_class
     searched_class = params.require(:npc)['dnd_class']
+    searched_class_arr = searched_class.split
+    # checks to see if any matching values in arrays; returns false if there's
+    # a matching value; returns true if no matching values.
+    if !(RaceClassArrays::ARTIFICER & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::ARTIFICER)
+
+      render json: @npcs
+    elsif !(RaceClassArrays::BARBARIAN & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::BARBARIAN)
+
+      render json: @npcs
+    elsif !(RaceClassArrays::BARD & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::BARD)
+
+      render json: @npcs
+    elsif !(RaceClassArrays::CLERIC & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::CLERIC)
+
+      render json: @npcs
+    elsif !(RaceClassArrays::DRUID & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::DRUID)
+
+      render json: @npcs
+    elsif !(RaceClassArrays::FIGHTER & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::FIGHTER)
+
+      render json: @npcs
+    elsif !(RaceClassArrays::MONK & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::MONK)
+
+      render json: @npcs
+    elsif !(RaceClassArrays::MYSTIC & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::MYSTIC)
+
+      render json: @npcs
+    elsif !(RaceClassArrays::PALADIN & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::PALADIN)
+
+      render json: @npcs
+    elsif !(RaceClassArrays::RANGER & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::RANGER)
+
+      render json: @npcs
+    elsif !(RaceClassArrays::ROGUE & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::ROGUE)
+
+      render json: @npcs
+    elsif !(RaceClassArrays::SORCERER & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::SORCERER)
+
+      render json: @npcs
+    elsif !(RaceClassArrays::WARLOCK & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::WARLOCK )
+
+      render json: @npcs
+    elsif !(RaceClassArrays::WIZARD & searched_class_arr).empty?
+      @npcs = Npc.where(dnd_class: RaceClassArrays::WIZARD)
+
+      render json: @npcs
+    else
     @npcs = Npc.where(dnd_class: searched_class)
 
     render json: @npcs
+    end
   end
 
   def search_by_challenge_rating
---
+++ db/race_class_arrays.rb
@@ -0,0 +1,190 @@
+# frozen_string_literal: true
+
+# A class to contain race/class arrays for search logic
+
+class RaceClassArrays
+
+  ARTIFICER = ['Artificer', 'artificer', 'Alchemist', 'alchemist',
+               'gunsmith', 'Gunsmith']
+
+  BARBARIAN = ['Barbarian', 'barbarian', 'berserker', 'Berserker',
+               'Totem Warrior', 'totem warrior', 'Ancestral Guardian',
+               'ancestral guardian', 'storm herald', 'Storm Herald',
+               'Zealot', 'zealot', 'Battlerager', 'battlerager']
+
+  BARD = ['Bard', 'bard', 'College', 'college']
+
+  CLERIC = ['Cleric', 'cleric', 'Arcana', 'arcana', 'Death', 'death',
+            'Forge', 'forge', 'Grave', 'grave', 'Knowledge',
+            'knowledge', 'Life', 'life', 'Light', 'light', 'Nature',
+            'nature', 'Tempest', 'tempest', 'trickery', 'Trickery',
+            'War', 'war']
+
+  DRUID = ['Druid', 'druid', 'Circle', 'circle']
+
+  FIGHTER = ['Fighter', 'fighter', 'Champion', 'champion',
+             'Battle Master', 'battle master', 'Eldritch Knight',
+             'eldritch knight', 'Arcane Archer', 'arcane archer',
+             'Cavalier', 'cavalier', 'Samurai', 'samurai',
+             'Monster Hunter', 'monster hunter', 'Sharpshooter',
+             'sharpshooter', 'brute', 'Brute']
+
+  MONK = ['Monk', 'monk']
+
+  MYSTIC = ['Mystic', 'mystic', 'Order of', 'order of', 'Order',
+            'order']
+
+  PALADIN = ['Paladin', 'paladin']
+
+  RANGER = ['Ranger', 'ranger', 'Hunter', 'hunter', 'Beast Master',
+            'beast master', 'gloom stalker', 'Gloom Stalker',
+            'Horizon Walker', 'horizon walker', 'monster slayer',
+            'Monster Slayer', 'Primeval Guardian', 'primeval guardian']
+
+  ROGUE = ['Rogue', 'rogue', 'thief', 'Thief', 'Assassin', 'assassin',
+           'arcane trickster', 'Arcane Trickster', 'Inquisitive',
+           'inquisitive', 'Mastermind', 'mastermind', 'Scout', 'scout',
+           'Swashbuckler', 'swashbuckler']
+
+  SORCERER = ['Sorcerer', 'sorcerer', 'Draconic', 'draconic', 'Wild',
+              'Wild', 'Divine Soul', 'divine soul', 'Shadow Magic',
+              'shadow magic', 'Storm Sorcery', 'storm sorcery',
+              'Phoenix Sorcery', 'phoenix sorcery', 'Sea Sorcery',
+              'sea sorcery', 'Stone Sorcery', 'stone sorcery']
+
+  WARLOCK = ['Warlock', 'warlock', 'Archfey', 'archfey', 'Fey', 'fey',
+            'Celestial', 'celestial', 'Fiend', 'fiend',
+            'Great Old One', 'Hexblade', 'hexblade', 'Raven Queen',
+            'raven queen', 'Seeker', 'seeker', 'Undying', 'undying',
+            'Pact of', 'pact of']
+
+  WIZARD = ['Wizard', 'wizard', 'Abjurer', 'abjurer', 'abjuration',
+            'Abjuration', 'Bladesinger', 'Bladesinging', 'bladesinger',
+            'bladesinging', 'Conjuror', 'conjuror', 'Conjuration',
+            'conjuratoon', 'Diviner', 'diviner', 'Divination',
+            'divination', 'Enchanter', 'enchanter', 'Evocatator',
+            'evocatator', 'Evocateur', 'evocateur', 'Evocation',
+            'evocation', 'Illusionist', 'illusionist', 'Illusion',
+            'illusion', 'Lore Master', 'lore master', 'Necromancer',
+            'necromancer', 'necromancy', 'Necromancy', 'Technomancy',
+            'technomany', 'Technomancer', 'technomancer',
+            'theurgy', 'Theurgy', 'Theurgist', 'theurgist',
+            'Transmutation', 'transmutation', 'transmuter',
+            'Transmuter', 'transmutor', 'Transmutor',
+            'war magic', 'War Magic', 'Mage', 'mage', 'magician',
+            'Magician']
+
+  # ARTIFICER = RaceClassArrays.new.artificer_array
+  # BARBARIAN = RaceClassArrays.new.barbarian_array
+  # BARD = RaceClassArrays.new.bard_array
+  # CLERIC = RaceClassArrays.new.cleric_array
+  # DRUID = RaceClassArrays.new.druid_array
+  # FIGHTER = RaceClassArrays.new.fighter_array
+  # MONK = RaceClassArrays.new.monk_array
+  # MYSTIC = RaceClassArrays.new.mystic_array
+  # PALADIN = RaceClassArrays.new.paladin_array
+  # RANGER = RaceClassArrays.new.ranger_array
+  # ROGUE = RaceClassArrays.new.rogue_array
+  # SORCERER = RaceClassArrays.new.sorcerer_array
+  # WARLOCK = RaceClassArrays.new.warlock_array
+  # WIZARD = RaceClassArrays.new.wizard_array
+  #
+  # def artificer_array
+  #   @artificer_array = ['Artificer', 'artificer', 'Alchemist', 'alchemist',
+  #                       'gunsmith', 'Gunsmith']
+  # end
+  #
+  # def barbarian_array
+  #   @barbarian_array = ['Barbarian', 'barbarian', 'berserker', 'Berserker',
+  #                       'Totem Warrior', 'totem warrior', 'Ancestral Guardian',
+  #                       'ancestral guardian', 'storm herald', 'Storm Herald',
+  #                       'Zealot', 'zealot', 'Battlerager', 'battlerager']
+  # end
+  #
+  # def bard_array
+  #   @bard_array = ['Bard', 'bard', 'College', 'college']
+  # end
+  #
+  # def cleric_array
+  #   @cleric_array = ['Cleric', 'cleric', 'Arcana', 'arcana', 'Death', 'death',
+  #                    'Forge', 'forge', 'Grave', 'grave', 'Knowledge',
+  #                    'knowledge', 'Life', 'life', 'Light', 'light', 'Nature',
+  #                    'nature', 'Tempest', 'tempest', 'trickery', 'Trickery',
+  #                    'War', 'war']
+  # end
+  #
+  # def druid_array
+  #   @druid_array = ['Druid', 'druid', 'Circle', 'circle']
+  # end
+  #
+  # def fighter_array
+  #   @fighter_array = ['Fighter', 'fighter', 'Champion', 'champion',
+  #                     'Battle Master', 'battle master', 'Eldritch Knight',
+  #                     'eldritch knight', 'Arcane Archer', 'arcane archer',
+  #                     'Cavalier', 'cavalier', 'Samurai', 'samurai',
+  #                     'Monster Hunter', 'monster hunter', 'Sharpshooter',
+  #                     'sharpshooter', 'brute', 'Brute']
+  # end
+  #
+  # def monk_array
+  #   @monk_array = ['Monk', 'monk']
+  # end
+  #
+  # def mystic_array
+  #   @mystic_array = ['Mystic', 'mystic', 'Order of', 'order of', 'Order',
+  #                    'order']
+  # end
+  #
+  # def paladin_array
+  #   @paladin_array = ['Paladin', 'paladin']
+  # end
+  #
+  # def ranger_array
+  #   @ranger_array = ['Ranger', 'ranger', 'Hunter', 'hunter', 'Beast Master',
+  #                    'beast master', 'gloom stalker', 'Gloom Stalker',
+  #                    'Horizon Walker', 'horizon walker', 'monster slayer',
+  #                    'Monster Slayer', 'Primeval Guardian', 'primeval guardian']
+  # end
+  #
+  # def rogue_array
+  #   @rogue_array = ['Rogue', 'rogue', 'thief', 'Thief', 'Assassin', 'assassin',
+  #                   'arcane trickster', 'Arcane Trickster', 'Inquisitive',
+  #                   'inquisitive', 'Mastermind', 'mastermind', 'Scout', 'scout',
+  #                   'Swashbuckler', 'swashbuckler']
+  # end
+  #
+  # def sorcerer_array
+  #   @sorcerer_array = ['Sorcerer', 'sorcerer', 'Draconic', 'draconic', 'Wild',
+  #                      'Wild', 'Divine Soul', 'divine soul', 'Shadow Magic',
+  #                      'shadow magic', 'Storm Sorcery', 'storm sorcery',
+  #                      'Phoenix Sorcery', 'phoenix sorcery', 'Sea Sorcery',
+  #                      'sea sorcery', 'Stone Sorcery', 'stone sorcery']
+  # end
+  #
+  # def warlock_array
+  #   @warlock_array = ['Warlock', 'warlock', 'Archfey', 'archfey', 'Fey', 'fey',
+  #                     'Celestial', 'celestial', 'Fiend', 'fiend',
+  #                     'Great Old One', 'Hexblade', 'hexblade', 'Raven Queen',
+  #                     'raven queen', 'Seeker', 'seeker', 'Undying', 'undying',
+  #                     'Pact of', 'pact of']
+  # end
+  #
+  # def wizard_array
+  #   @wizard_array = ['Wizard', 'wizard', 'Abjurer', 'abjurer', 'abjuration',
+  #                    'Abjuration', 'Bladesinger', 'Bladesinging', 'bladesinger',
+  #                    'bladesinging', 'Conjuror', 'conjuror', 'Conjuration',
+  #                    'conjuratoon', 'Diviner', 'diviner', 'Divination',
+  #                    'divination', 'Enchanter', 'enchanter', 'Evocatator',
+  #                    'evocatator', 'Evocateur', 'evocateur', 'Evocation',
+  #                    'evocation', 'Illusionist', 'illusionist', 'Illusion',
+  #                    'illusion', 'Lore Master', 'lore master', 'Necromancer',
+  #                    'necromancer', 'necromancy', 'Necromancy', 'Technomancy',
+  #                    'technomany', 'Technomancer', 'technomancer',
+  #                    'theurgy', 'Theurgy', 'Theurgist', 'theurgist',
+  #                    'Transmutation', 'transmutation', 'transmuter',
+  #                    'Transmuter', 'transmutor', 'Transmutor',
+  #                    'war magic', 'War Magic', 'Mage', 'mage', 'magician',
+  #                    'Magician']
+  # end
+
+end
