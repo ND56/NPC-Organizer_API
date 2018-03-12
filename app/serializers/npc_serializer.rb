ebfd5869f7f5743e2df862f6cbd7460b071ce84b
ND56 <ndrew56@gmail.com>
2018-03-12 15:09:51 -0400

    successfully changed serializer to send created_at information

--- app/serializers/npc_serializer.rb
+++ app/serializers/npc_serializer.rb
@@ -1,7 +1,7 @@
 class NpcSerializer < ActiveModel::Serializer
   attributes :id, :name, :race, :challenge_rating, :traits, :notes,
              :private, :dnd_class, :HP, :AC, :ability_modifiers,
-             :spells_abilities, :items, :level
+             :spells_abilities, :items, :level, :created_at, :updated_at
   has_one :user
   # consider whether I should just include :user_id in the attributes list
   # this might allow people to access sensitive information
