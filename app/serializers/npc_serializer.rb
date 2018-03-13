0bd28899f804a37889c9462a17af90b45c9ba3ab
ND56 <ndrew56@gmail.com>
2018-03-13 13:29:30 -0400

    npc serializer now sends users_liked information in response

--- app/serializers/npc_serializer.rb
+++ app/serializers/npc_serializer.rb
@@ -3,6 +3,7 @@ class NpcSerializer < ActiveModel::Serializer
              :private, :dnd_class, :HP, :AC, :ability_modifiers,
              :spells_abilities, :items, :level, :created_at, :updated_at
   has_one :user
+  has_many :liking_users
   # consider whether I should just include :user_id in the attributes list
   # this might allow people to access sensitive information
 end
